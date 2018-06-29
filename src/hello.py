17ea0adbc8fe2e1c858aa2e21ce0197bf29b5cdf
Victor Augusto <victoraweb@gmail.com>
2018-06-29 08:09:32 -0300

    Add route with params

--- src/hello.py
+++ src/hello.py
@@ -3,8 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Hello world!'
+
+@app.route('/users/<username>')
+def username(username):
+    return f'Hello {username}'
+
+@app.route('/posts/<int:post_id>')
+def get_post(post_id):
+    return 'Post ' + str(post_id)
+
+@app.route('/hello')
 def hello_world():
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     i = 3
     i += 1
     visited = i
