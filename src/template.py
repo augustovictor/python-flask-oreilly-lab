f266dd887470bfe580e59a28658594d152420196
Victor Augusto <victoraweb@gmail.com>
2018-06-30 09:56:26 -0300

    [FEAT] Add flahs messages and login validation

--- src/template.py
+++ src/template.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'MY-SECRET'
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def main_page(name=None):
+@app.route('/welcome/<name>')
+def welcome(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def signin():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if valid_login(username, password):
+            flash('Successfully logged in')
+            return redirect(url_for('welcome', name=username))
+        else:
+            error = 'Invalid credentials'
+    return render_template('login.html', error=error)
+
+def valid_login(username, password):
+    return username == password
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
