510885488279614a12aef3959e2223f6cd2d9cf3
Victor Augusto <victoraweb@gmail.com>
2018-06-30 14:41:48 -0300

    [FEAT] Add cookie and session support

--- src/template.py
+++ src/template.py
@@ -1,27 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
 
 app = Flask(__name__)
 app.secret_key = 'MY-SECRET'
 
-@app.route('/welcome/<name>')
+@app.route('/welcome')
 def welcome(name=None):
-    return render_template('hello.html', name=name)
+    username = request.cookies.get('username')
+    if username:
+        return render_template('hello.html', name=username)
+    else:
+        flash('You are not logged in')
+        return redirect(url_for('signin'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def signin():
-    error = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if valid_login(username, password):
             flash('Successfully logged in')
-            return redirect(url_for('welcome', name=username))
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('username', username)
+            return response
+            
         else:
             flash('Invalid credentials')
     return render_template('login.html')
 
 def valid_login(username, password):
     return username == password
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('signin')))
+    response.set_cookie('username', '', expires=0)
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
